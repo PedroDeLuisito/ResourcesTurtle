e7cd3a82b263f1e05759313dd5d64a9b89aaa340
PedroDeLuisito <pilou.desfaits@gmail.com>
2022-08-18 13:26:19 +0200

    Add all Tier 1 Turtle Egg
    Add :
    - coal
    -gravel
    -rainbow
    -rocky
    -woody
    -zombie

--- src/main/java/com/pedrodeluisito/rturtle/item/ModItems.java
+++ src/main/java/com/pedrodeluisito/rturtle/item/ModItems.java
@@ -15,10 +15,30 @@ public class ModItems {
     //Gold Turtle Egg
     public static final RegistryObject<Item> GOLD_TURTLE_EGG = ITEMS.register("gold_turtle_egg",
             ()-> new Item (new Item.Properties().group(ModItemGroup.TURTLE_GROUP)));
-
     //Redstone Turtle Egg
     public static final RegistryObject<Item> REDSTONE_TURTLE_EGG = ITEMS.register("redstone_turtle_egg",
             ()-> new Item (new Item.Properties().group(ModItemGroup.TURTLE_GROUP)));
+    //Coal Turtle Egg
+    public static final RegistryObject<Item> COAL_TURTLE_EGG = ITEMS.register("coal_turtle_egg",
+            ()-> new Item (new Item.Properties().group(ModItemGroup.TURTLE_GROUP)));
+    //Gravel Turtle Egg
+    public static final RegistryObject<Item> GRAVEL_TURTLE_EGG = ITEMS.register("gravel_turtle_egg",
+            ()-> new Item (new Item.Properties().group(ModItemGroup.TURTLE_GROUP)));
+    //Rainbow Turtle Egg
+    public static final RegistryObject<Item> RAINBOW_TURTLE_EGG = ITEMS.register("rainbow_turtle_egg",
+            ()-> new Item (new Item.Properties().group(ModItemGroup.TURTLE_GROUP)));
+    //Rocky Turtle Egg
+    public static final RegistryObject<Item> ROCKY_TURTLE_EGG = ITEMS.register("rocky_turtle_egg",
+            ()-> new Item (new Item.Properties().group(ModItemGroup.TURTLE_GROUP)));
+    //Woody Turtle Egg
+    public static final RegistryObject<Item> WOODY_TURTLE_EGG = ITEMS.register("woody_turtle_egg",
+            ()-> new Item (new Item.Properties().group(ModItemGroup.TURTLE_GROUP)));
+    //Zombie Turtle Egg
+    public static final RegistryObject<Item> ZOMBIE_TURTLE_EGG = ITEMS.register("zombie_turtle_egg",
+            ()-> new Item (new Item.Properties().group(ModItemGroup.TURTLE_GROUP)));
+
+
+
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
